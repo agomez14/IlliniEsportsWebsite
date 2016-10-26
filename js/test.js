256aa013837d5c23c06f24999e28d77685272d03
Armando Gomez <agomez14@illinois.edu>
2016-10-26 02:11:08 -0500

    New update

--- js/test.js
+++ js/test.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-	$('#carousel').slick({
-		autoplay: true,
-		arrows: false,
-		adaptiveHeight: true,
-		autoplaySpeed: 3000,
-		fade: true,
-		speed: 750,
-	});
+  $('.hero-slick').slick({
+    autoplay: true,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+  });
+  $('.sponsor-slick').slick({
+  	infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	autoplay: true,
+  	arrows: false,
+  	speed: 900,
+  });
 });
